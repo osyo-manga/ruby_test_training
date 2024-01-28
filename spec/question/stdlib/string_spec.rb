7e06828a2d4dacb12789aee6e07c4e8eba063e73
manga_osyo <manga.osyo@gmail.com>
2024-01-28 23:17:10 +0900

    String#rjust を追加

--- spec/question/stdlib/string_spec.rb
+++ spec/question/stdlib/string_spec.rb
@@ -615,4 +615,51 @@ RSpec.describe String do
       it { expect { subject }.to raise_error ___ }
     end
   end
+
+  describe "#rjust" do
+    subject { string.rjust(*args) }
+
+    let(:string) { "foo" }
+    let(:args) { [width] }
+
+    xcontext "`width` に `6` を渡した場合" do
+      let(:width) { 6 }
+      it { is_expected.to eq ___ }
+    end
+
+    xcontext "`width` に `5` を渡した場合" do
+      let(:width) { ___ }
+      it { is_expected.to eq "  foo" }
+    end
+
+    xcontext "`width` に `4` を渡した場合" do
+      let(:width) { 4 }
+      it { is_expected.to eq ___ }
+    end
+
+    xcontext "`width` に `2` を渡した場合" do
+      let(:width) { 2 }
+      it { is_expected.to eq ___ }
+    end
+
+    xcontext "`width` に `0` を渡した場合" do
+      let(:width) { 0 }
+      it { is_expected.to eq ___ }
+    end
+
+    xcontext "`width` に `-1` を渡した場合" do
+      let(:width) { -1 }
+      it { is_expected.to eq ___ }
+    end
+
+    xcontext "第二引数に `*` を渡した場合" do
+      let(:args) { [8, "*"] }
+      it { is_expected.to eq ___ }
+    end
+
+    xcontext "引数を渡さなかった時" do
+      let(:args) { [] }
+      it { expect { subject }.to raise_error ___ }
+    end
+  end
 end
